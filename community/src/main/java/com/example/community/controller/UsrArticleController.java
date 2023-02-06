a7d622e7a21b9850e6a511e716eb36ee89d480db
jinsu291 <wlstn291@naver.com>
2023-02-07 00:20:55 +0900

    modify

--- community/src/main/java/com/example/community/controller/UsrArticleController.java
+++ community/src/main/java/com/example/community/controller/UsrArticleController.java
@@ -17,11 +17,20 @@ public class UsrArticleController {
     public UsrArticleController() {
         articlesLastId = 0;
         articles = new ArrayList<>();
+
+        makeTestData();
     }
 
-    @RequestMapping("/usr/article/doAdd")
-    @ResponseBody
-    public Article doAdd(String title, String body) {
+    private void makeTestData() {
+        for (int i = 1; i <= 10; i++) {
+            String title = "제목 " + i;
+            String body = "내용 " + i;
+
+            writeArticle(title, body);
+        }
+    }
+
+    public Article writeArticle(String title, String body) {
         int id = articlesLastId + 1;
         Article article = new Article(id, title, body);
 
@@ -31,9 +40,47 @@ public class UsrArticleController {
         return article;
     }
 
+    @RequestMapping("/usr/article/doAdd")
+    @ResponseBody
+    public Article doAdd(String title, String body) {
+        Article article = writeArticle(title, body);
+
+        return article;
+    }
+
     @RequestMapping("/usr/article/getArticles")
     @ResponseBody
     public List<Article> getArticles() {
         return articles;
+    }
+
+    @RequestMapping("/usr/article/doDelete")
+    @ResponseBody
+    public String doDelete(int id) {
+        Article article = getArticle(id);
+
+        if ( article == null ) {
+            return id + "번 게시물이 존재하지 않습니다.";
+        }
+
+        deleteArticle(id);
+
+        return id + "번 게시물을 삭제하였습니다.";
+    }
+
+    private void deleteArticle(int id) {
+        Article article = getArticle(id);
+
+        articles.remove(article);
+    }
+
+    private Article getArticle(int id) {
+        for ( Article article : articles ) {
+            if ( article.getId() == id ) {
+                return article;
+            }
+        }
+
+        return null;
     }
 }
