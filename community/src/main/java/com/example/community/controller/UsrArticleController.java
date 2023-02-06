67004e60692cf0648670083beb681b3d2fb2d111
jinsu291 <wlstn291@naver.com>
2023-02-07 00:31:38 +0900

    make ArticleService, divide

--- community/src/main/java/com/example/community/controller/UsrArticleController.java
+++ community/src/main/java/com/example/community/controller/UsrArticleController.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.example.community.entity.Article;
+import com.example.community.service.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,46 +13,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UsrArticleController {
-    private int articlesLastId;
-    private List<Article> articles;
+    @Autowired
+    private ArticleService articleService;
 
-    public UsrArticleController() {
-        articlesLastId = 0;
-        articles = new ArrayList<>();
-
-        makeTestData();
-    }
-
-    private void modifyArticle(int id, String title, String body) {
-        Article article = getArticle(id);
-
-        article.setTitle(title);
-        article.setBody(body);
-    }
-
-    private void makeTestData() {
-        for (int i = 1; i <= 10; i++) {
-            String title = "제목 " + i;
-            String body = "내용 " + i;
-
-            writeArticle(title, body);
-        }
-    }
-
-    public Article writeArticle(String title, String body) {
-        int id = articlesLastId + 1;
-        Article article = new Article(id, title, body);
-
-        articles.add(article);
-        articlesLastId = id;
-
-        return article;
-    }
 
     @RequestMapping("/usr/article/doAdd")
     @ResponseBody
     public Article doAdd(String title, String body) {
-        Article article = writeArticle(title, body);
+        Article article = articleService.writeArticle(title, body);
 
         return article;
     }
@@ -58,50 +28,46 @@ public class UsrArticleController {
     @RequestMapping("/usr/article/getArticles")
     @ResponseBody
     public List<Article> getArticles() {
-        return articles;
+        return articleService.getArticles();
     }
 
     @RequestMapping("/usr/article/doDelete")
     @ResponseBody
     public String doDelete(int id) {
-        Article article = getArticle(id);
+        Article article = articleService.getArticle(id);
 
         if ( article == null ) {
             return id + "번 게시물이 존재하지 않습니다.";
         }
 
-        deleteArticle(id);
+        articleService.deleteArticle(id);
 
         return id + "번 게시물을 삭제하였습니다.";
-    }
-
-    private void deleteArticle(int id) {
-        Article article = getArticle(id);
-
-        articles.remove(article);
-    }
-
-    private Article getArticle(int id) {
-        for ( Article article : articles ) {
-            if ( article.getId() == id ) {
-                return article;
-            }
-        }
-
-        return null;
     }
 
     @RequestMapping("/usr/article/doModify")
     @ResponseBody
     public String doModify(int id, String title, String body) {
-        Article article = getArticle(id);
+        Article article = articleService.getArticle(id);
 
         if ( article == null ) {
             return id + "번 게시물이 존재하지 않습니다.";
         }
 
-        modifyArticle(id, title, body);
+        articleService.modifyArticle(id, title, body);
 
         return id + "번 게시물을 수정하였습니다.";
+    }
+
+    @RequestMapping("/usr/article/getArticle")
+    @ResponseBody
+    public Object getArticleAction(int id) {
+        Article article = articleService.getArticle(id);
+
+        if ( article == null ) {
+            return id + "번 게시물은 존재하지 않습니다.";
+        }
+
+        return article;
     }
 }
---
+++ community/src/main/java/com/example/community/service/ArticleService.java
@@ -0,0 +1,66 @@
+package com.example.community.service;
+
+import com.example.community.entity.Article;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ArticleService {
+    private int articlesLastId;
+    private List<Article> articles;
+
+    public ArticleService() {
+        articlesLastId = 0;
+        articles = new ArrayList<>();
+
+        makeTestData();
+    }
+
+    private void makeTestData() {
+        for (int i = 1; i <= 10; i++) {
+            String title = "제목 " + i;
+            String body = "내용 " + i;
+
+            writeArticle(title, body);
+        }
+    }
+
+    public Article writeArticle(String title, String body) {
+        int id = articlesLastId + 1;
+        Article article = new Article(id, title, body);
+
+        articles.add(article);
+        articlesLastId = id;
+
+        return article;
+    }
+
+    public Article getArticle(int id) {
+        for ( Article article : articles ) {
+            if ( article.getId() == id ) {
+                return article;
+            }
+        }
+
+        return null;
+    }
+
+    public void modifyArticle(int id, String title, String body) {
+        Article article = getArticle(id);
+
+        article.setTitle(title);
+        article.setBody(body);
+    }
+
+    public void deleteArticle(int id) {
+        Article article = getArticle(id);
+
+        articles.remove(article);
+    }
+
+    public List<Article> getArticles() {
+        return articles;
+    }
+}
